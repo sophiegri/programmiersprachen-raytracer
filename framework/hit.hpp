02303eab1e4603575c4cb25360aad2f134a41eca
sophiegri <sophiegrimme@gmx.de>
2018-09-26 23:34:22 +0200

    new hit and plane struct

---
+++ framework/hit.hpp
@@ -0,0 +1,11 @@
+#ifndef HIT_HPP 
+#define HIT_HPP 
+
+struct Hit {
+
+    glm::vec3 position;
+    glm::vec3 normal;
+     
+};
+
+#endif 
---
+++ framework/plane.hpp
@@ -0,0 +1,11 @@
+#ifndef PLANE_HPP
+#define PLANE_HPP
+#include <glm/vec3.hpp>
+
+struct Plane {
+
+    glm::vec3 origin; 
+    glm::vec3 normal; 
+};
+
+#endif 
